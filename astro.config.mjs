0eadda120807c1a87d8ed3f11ef3af0a4fa6b16f
66Leo66 <leoli@6leo6.com>
2023-07-28 15:51:47 +0000

    fix add back react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,61 +4,42 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-
 import remarkParse from "remark-parse";
 import remarkMermaid from "remark-mermaidjs";
 import torchlight from "remark-torchlight";
 import remarkMath from "remark-math";
-
 import rehypeKatex from "rehype-katex";
 import "katex/contrib/mhchem";
 
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), react()],
   markdown: {
     syntaxHighlight: false,
-    remarkPlugins: [
-      remarkParse,
-      remarkMermaid,
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-      [
-        torchlight,
-        {
-          token: process.env.TORCHLIGHT_TOKEN,
-          theme: "nord",
-        },
-      ],
-      remarkMath,
-    ],
-    rehypePlugins: [
-      rehypeKatex
-    ],
+    remarkPlugins: [remarkParse, remarkMermaid, remarkToc, [remarkCollapse, {
+      test: "Table of contents"
+    }], [torchlight, {
+      token: process.env.TORCHLIGHT_TOKEN,
+      theme: "nord"
+    }], remarkMath],
+    rehypePlugins: [rehypeKatex]
   },
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
+      exclude: ["@resvg/resvg-js"]
+    }
   },
   experimental: {
-    assets: true,
+    assets: true
   },
   image: {
-    service: sharpImageService(),
-  },
+    service: sharpImageService()
+  }
 });
