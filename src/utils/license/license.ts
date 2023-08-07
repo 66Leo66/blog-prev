fe3a6e49dc5f45fd185d44d541961348c62592ae
Leo Li <leoli@6leo6.com>
2023-08-07 18:52:46 +0800

    maybe fix, untested

--- src/utils/license/license.ts
+++ src/utils/license/license.ts
@@ -1,30 +1,14 @@
-import { parse } from "smol-toml";
-import fs from "fs";
-import { join } from "path";
+import { licenseMap } from "./licenseList";
 
-export interface License {
-  id: string;
-  name: string;
-  url?: string;
-}
-
-// parse from license.toml(read file)
-const licenseListFile = new URL("./license.toml", import.meta.url);
-const licenseList = parse(fs.readFileSync(licenseListFile, "utf-8"));
-
-// get license by id
-export function getLicense(id: string): License {
-  console.log(licenseList);
-  if (!licenseList[id]) {
-    return getLicense("default");
-  } else if (licenseList[id]["aliasTo"]) {
-    return getLicense(licenseList[id]["aliasTo"]);
+export function getLicense(id: string) {
+  const license = licenseMap[id];
+  if (license) {
+    if ("aliasTo" in license) {
+      return getLicense(license.aliasTo);
+    } else {
+      return license;
+    }
   } else {
-    const license: License = {
-      id: id,
-      name: licenseList[id]["name"],
-      url: licenseList[id]["url"],
-    };
-    return license;
+    return getLicense("default");
   }
 }
---
+++ src/utils/license/licenseList.ts
@@ -0,0 +1,36 @@
+export type License = {
+    name: string;
+    fullName: string;
+    url?: string;
+}
+
+export type LicenseAlias = {
+    aliasTo: string;
+}
+
+export type LicenseListItem = License | LicenseAlias;
+
+export const licenseMap: Record<string, LicenseListItem> = {
+    "cc-by-ns-4.0": {
+        name: "CC BY-NC 4.0",
+        fullName: "Creative Commons Attribution-NonCommercial 4.0 International",
+        url: "https://creativecommons.org/licenses/by-nc/4.0/"
+    },
+    "cc-by-nc-sa-4.0": {
+        name: "CC BY-NC-SA 4.0",
+        fullName: "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International",
+        url: "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+    },
+    "cc0": {
+        name: "CC0",
+        fullName: "Creative Commons Zero v1.0 Universal",
+        url: "https://creativecommons.org/publicdomain/zero/1.0/"
+    },
+    "copyright": {
+        name: "All Rights Reserved",
+        fullName: "All Rights Reserved"
+    },
+    "default": {
+        aliasTo: "copyright"
+    }
+}
