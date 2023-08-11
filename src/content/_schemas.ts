7a6fbdc2f0ea698abb88fd70302144b8f296635f
Leo Li <33322229+66Leo66@users.noreply.github.com>
2023-08-11 17:30:57 +0800

    更新 _schemas.ts

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -3,7 +3,7 @@ import { z } from "astro:content";
 export const blogSchema = z
   .object({
     author: z.string().optional(),
-    pubDatetime: z.date(),
+    pubDatetime: z.string().datetime({ offset: true }),
     title: z.string(),
     postSlug: z.string().optional(),
     featured: z.boolean().optional(),
