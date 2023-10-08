990e55ff4fbfb91e331956e57d2756b9c38e8f86
Leo Li <33322229+66Leo66@users.noreply.github.com>
2023-10-08 18:55:07 +0800

    Delete src/pages/_pagefind directory

--- src/pages/_pagefind/pagefind.js.ts
+++
@@ -1,8 +0,0 @@
-// a fake pagefind endpoint to prevent broken dev
-import type { APIContext } from "astro";
-
-export async function get({}: APIContext) {
-  return {
-    body: "export const search = () => {return {results: []}}",
-  };
-}
